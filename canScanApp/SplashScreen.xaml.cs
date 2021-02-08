d984a8e1f440579eb0cb92e3f0da80d49ddecdc5
Ana Ruiz <ana.ruizfz@udlap.mx>
2021-02-07 21:29:14 -0600

    Se añadió splash screen y controles de ventana

---
+++ canScanApp/SplashScreen.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace canScanApp
+{
+    /// <summary>
+    /// Lógica de interacción para splashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 2);
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+
+            timer.Stop();
+            this.Close();
+        }
+    }
+}
