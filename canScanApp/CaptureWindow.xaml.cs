42d08a3b6772155322ca846e920a725418d1fd74
Ana Ruiz <ana.ruizfz@udlap.mx>
2021-03-02 00:40:18 -0600

    Textbox only takes numbers and save formats were added

--- canScanApp/CaptureWindow.xaml.cs
+++ canScanApp/CaptureWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.Text.RegularExpressions;
 
 
 namespace canScanApp
@@ -31,8 +32,42 @@ namespace canScanApp
         private void SaveFile(object sender, RoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
+            String extension = fileType.Text.Split('.')[1].Split(' ')[0];
+            saveFileDialog.DefaultExt = extension.Substring(0, 1) + extension.Substring(1);
+            saveFileDialog.FileName = "CanScanData";
+            saveFileDialog.Filter = "Excel Worksheets|*.xls;*.xlsx|txt files (*.txt)|*.txt|All files (*.*)|*.*";
             if (saveFileDialog.ShowDialog() == true)
-                File.WriteAllText(saveFileDialog.FileName, timeGiven.Text);
+                File.WriteAllText(saveFileDialog.FileName, textbox1.Text);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Textbooxx(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
+        }
+
+        private void Textboxx(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = new Regex("[^0-9]+").IsMatch(e.Text);
         }
     }
 }
